4e535a4383400ac46603abb2d9c7454ee63cfbba
Sathishkumar S <ssathishkumar@75f.io>
2025-04-15 16:00:34 +0530

    Update for shell layout - 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+plugins {
+    kotlin("android") version "1.9.22"
+}
 tasks.register("clean") {
     subprojects.forEach {
         dependsOn(":app:${it.name}:clean")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("MagiskPlugin")
+    kotlin("android") version "1.9.22"
 }
 
 tasks.register("clean", Delete::class) {
